c2508d53288abbba5508840f6750a1b334de914d
buynlt0101 <buynlt0101@gmail.com>
2023-08-05 11:40:53 +0700

    ss

--- .circleci/infrastructure/update.sh
+++ .circleci/infrastructure/update.sh
@@ -1,1 +1,15 @@
-aws cloudformation update-stack --stack-name $1 --template-body file://$2  --parameters file://$3 --region=us-east-1 --capabilities CAPABILITY_NAMED_IAM
+#!/bin/bash
+output=$(aws cloudformation update-stack --stack-name $1 --template-body file://$2  --parameters file://$3 --region=us-east-1 --capabilities CAPABILITY_NAMED_IAM 2>&1)
+RESULT=$?
+
+if [ $RESULT -eq 0 ]; then
+  echo "$output"
+else
+  if [[ "$output" == *"No updates are to be performed"* ]]; then
+    echo "No cloudformation updates are to be performed."
+    exit 0
+  else
+    echo "$output"
+    exit 1
+  fi
+fi
