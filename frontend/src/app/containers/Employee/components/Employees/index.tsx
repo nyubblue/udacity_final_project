e54284d87ae1f614b2b5c059ae9a83c658e03a29
buynlt0101 <buynlt0101@gmail.com>
2023-08-08 01:34:01 +0700

    test rollback

--- frontend/src/app/containers/Employee/components/Employees/index.tsx
+++ frontend/src/app/containers/Employee/components/Employees/index.tsx
@@ -205,7 +205,7 @@ export class Employees extends React.Component<
         <SearchBar searchInput={generalSearch} />
         <div className={`${style['g-sectionbar']} ${style['grid-x']}`}>
           <Breadcrumb
-            rootPathName='View & Manage Employees V1'
+            rootPathName='View & Manage Employees V2'
             isSecondaryPage={false}
           >
             <li>
