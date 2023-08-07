3e4b55229a138b5d586367dc8370a840bd3b1201
buynlt0101 <buynlt0101@gmail.com>
2023-08-07 22:00:28 +0700

    ss

--- frontend/src/app/components/ActionsMenu/index.tsx
+++ frontend/src/app/components/ActionsMenu/index.tsx
@@ -1,90 +1,92 @@
-import React from 'react';
-import '../../style.local.css';
-import style from '../../style.local.css';
-import { Button } from '../Button';
-import { RouteComponentProps } from 'react-router';
-import ContextMenu from 'axui-contextmenu';
-import './style.css';
-import { EmployeeActions } from 'app/containers/Employee/actions';
+import React from 'react'
+import '../../style.local.css'
+import style from '../../style.local.css'
+import { Button } from '../Button'
+import { RouteComponentProps } from 'react-router'
+import ContextMenu from 'axui-contextmenu'
+import './style.css'
+import { EmployeeActions } from 'app/containers/Employee/actions'
 
 export namespace ActionsMenu {
   export interface Props extends RouteComponentProps<void> {
-    employeeId: string;
-    isActive: boolean;
-    actions: EmployeeActions;
+    employeeId: string
+    isActive: boolean
+    actions: EmployeeActions
   }
 
   export interface State {
-    showMenu: boolean;
+    showMenu: boolean
   }
 }
 
 export class ActionsMenu extends React.Component<ActionsMenu.Props> {
-  state: ActionsMenu.State;
-  menu: ContextMenu;
+  state: ActionsMenu.State
+  menu: ContextMenu
 
-  constructor(props: ActionsMenu.Props) {
-    super(props);
+  constructor (props: ActionsMenu.Props) {
+    super(props)
 
     this.state = {
       showMenu: false,
-    };
+    }
 
     this.menu = new ContextMenu({
       id: 'basic',
-    });
+    })
   }
 
-  toggleShowMenu(event: React.FormEvent<HTMLFormElement>) {
-    event.preventDefault();
-    event.stopPropagation();
+  toggleShowMenu (event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+    event.stopPropagation()
     this.setState({
       showMenu: !this.state.showMenu,
-    });
+    })
   }
 
-  onMouseLeave() {
+  onMouseLeave () {
     this.setState({
       showMenu: false,
-    });
+    })
   }
 
   goToEditEmployee = () => {
-    this.props.history.push(`/employees/${this.props.employeeId}/edit`);
+    this.props.history.push(`/employees/${this.props.employeeId}/edit`)
   }
 
-  goToViewEmployee = () => { };
+  goToViewEmployee = () => {}
 
-  goToArchiveEmployee = () => { };
+  goToArchiveEmployee = () => {}
 
   handleContextMenu = (e: any) => {
-    e.preventDefault();
+    e.preventDefault()
 
     this.menu.setMenu([
       {
         label: 'View',
         click: () => {
-          this.props.history.push(`/employees/${this.props.employeeId}/view`);
+          this.props.history.push(`/employees/${this.props.employeeId}/view`)
         },
       },
       {
         label: 'Edit',
         click: () => {
-          this.props.history.push(`/employees/${this.props.employeeId}/edit`);
+          this.props.history.push(`/employees/${this.props.employeeId}/edit`)
         },
       },
       {
         label: !this.props.isActive ? 'Activate' : 'Deactivate',
         click: () => {
-          this.props.isActive ? this.props.actions.deactivateEmployee(this.props.employeeId) : this.props.actions.activateEmployee(this.props.employeeId);
-          this.props.actions.fetchEmployees();
+          this.props.isActive
+            ? this.props.actions.deactivateEmployee(this.props.employeeId)
+            : this.props.actions.activateEmployee(this.props.employeeId)
+          this.props.actions.fetchEmployees()
         },
       },
-    ]);
-    this.menu.popup({ x: e.pageX, y: e.pageY });
+    ])
+    this.menu.popup({ x: e.pageX, y: e.pageY })
   }
 
-  render() {
+  render () {
     return (
       <div onClick={this.handleContextMenu}>
         <Button>
@@ -93,6 +95,6 @@ export class ActionsMenu extends React.Component<ActionsMenu.Props> {
           </a>
         </Button>
       </div>
-    );
+    )
   }
 }
--- frontend/src/app/containers/Employee/components/EditEmployee/index.tsx
+++ frontend/src/app/containers/Employee/components/EditEmployee/index.tsx
@@ -356,7 +356,7 @@ export class EditEmployee extends Component<EditEmployee.Props, EditEmployee.Sta
     const birthdate = date || new Date();
     this.processInputChange(
       { birthdate: ('{birthdate}') },
-      () => this.props.actions.updateDateField(this.state.employeeId, { value: birthdate }, 'birthdate'), //thunk
+      () => this.props.actions.updateDateField(this.state.employeeId, { value: birthdate }, 'birthdate'), // thunk
     );
   }
 
@@ -364,16 +364,16 @@ export class EditEmployee extends Component<EditEmployee.Props, EditEmployee.Sta
     const effectiveDate = date || new Date();
     this.processInputChange(
       { effectiveDate: ('{effectiveDate}') },
-      () => this.props.actions.updateDateField(this.state.employeeId, { value: effectiveDate }, 'effectiveDate'), //thunk
+      () => this.props.actions.updateDateField(this.state.employeeId, { value: effectiveDate }, 'effectiveDate'), // thunk
       !isInputEmpty(effectiveDate.toString()),
-      `Effective Date is invalid`, //error notification
+      `Effective Date is invalid`, // error notification
     );
   }
 
   handleTagsChange = (newTags: Array<any>) => {
     this.processInputChange(
       { tags: newTags },
-      () => this.props.actions.updateStringField(this.state.employeeId, { value: JSON.stringify(newTags) }, 'tags'), //thunk
+      () => this.props.actions.updateStringField(this.state.employeeId, { value: JSON.stringify(newTags) }, 'tags'), // thunk
     );
   }
 
